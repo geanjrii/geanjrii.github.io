0ca9e4e2685a86511db624460ba1da100e65a275
Gean jr <gean.junior@academico.ufpb.br>
2025-01-15 16:07:47 -0300

    second test

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4055079336"
+    serviceWorkerVersion: "2608113958"
   }
 });
