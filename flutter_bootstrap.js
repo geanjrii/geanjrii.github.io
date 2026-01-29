f1e6df99300eceaae5789d0d6233ae25b4a9939f
Gean jr. <gean.junior@academico.ufpb.br>
2026-01-29 00:21:51 -0300

    chandged splash screan

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"b9afb03275c7cf04906cb5b94faa60005561fc
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "252835999" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3582885688" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
